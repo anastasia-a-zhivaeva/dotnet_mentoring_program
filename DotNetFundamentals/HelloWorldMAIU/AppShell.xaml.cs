692bd437a9829e473235ff97411ceb8e8ca6ebfb
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-04-25 13:29:13 -0400

    feat (HelloWorldMAUI): add MAUI Page to greet people by comma separated names

---
+++ DotNetFundamentals/HelloWorldMAIU/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldMAIU
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DotNetFundamentals/HelloWorldMAIU/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using HelloWorldLibrary;
+
+namespace HelloWorldMAIU
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            var greetingNames = Names.Text?.Split(',').Where(s => s.Length > 0).ToArray() ?? Array.Empty<string>();
+            Greetings.Text = String.Empty;
+
+            if (greetingNames.Length > 0)
+            {
+                foreach (var name in greetingNames) {
+                    Greetings.Text += Greeting.Greet(name);
+                    Greetings.Text += "\n";
+                }
+            }
+            else
+            {
+                Greetings.Text = Greeting.Greet();
+            }
+            SemanticScreenReader.Announce(Greetings.Text);
+        }
+    }
+}
