ea063045164f96fe6fb8d962bcf110c4b6e56121
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-04-27 21:56:41 -0400

    feat (DebuggerFundamentals): add DebbugerFundamentals solution and GetStartedDebugging project to learn debugging

---
+++ DebuggerFundamentals/DebugCSharpCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class ArrayExample
+{
+    static void Main()
+    {
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+            try
+            {
+                if (i > 5) throw new IndexOutOfRangeException();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        Console.ReadKey();
+    }
+
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
+    }
+}
