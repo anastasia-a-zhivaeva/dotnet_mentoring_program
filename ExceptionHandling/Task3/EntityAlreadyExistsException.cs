3e79e6077208bc2745893ce272129dd55b8a8715
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-05-17 21:53:48 -0400

    feat (ExceptionHandling): solve Task3

---
+++ ExceptionHandling/Task3/EntityAlreadyExistsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Task3
+{
+    [Serializable]
+    public class EntityAlreadyExistsException : Exception
+    {
+        public EntityAlreadyExistsException() : base() { }
+        public EntityAlreadyExistsException(string message) : base(message) { }
+        public EntityAlreadyExistsException(string message, Exception innerException) : base(message, innerException) { }
+        protected EntityAlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
