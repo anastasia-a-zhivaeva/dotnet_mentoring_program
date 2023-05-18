83738cff58ffd34bc05f12dc26afaa499e980fe0
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-05-17 20:38:31 -0400

    feat (ExceptionHandling): solve Task1

--- ExceptionHandling/Task1/Program.cs
+++ ExceptionHandling/Task1/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
+        private static readonly string exit = "exit";
+        private static readonly string clean = "clean";
+
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            StartMainLoop();
+        }
+
+        private static void StartMainLoop()
+        {
+            do 
+            {
+                var inputLine = ReadInputLineOrCommand();
+
+                if (inputLine == exit) break;
+
+                if (inputLine == clean)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                WriteFirstCharacter(inputLine);
+            } while (true);
+        }
+
+        private static string ReadInputLineOrCommand()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Type something to extract first character or '{0}' or '{1}':", exit, clean);
+            return Console.ReadLine();
+        }
+
+        private static void WriteFirstCharacter(string inputLine)
+        {
+            try
+            {
+                Console.WriteLine(inputLine[0]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Input line cannot be empty!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
