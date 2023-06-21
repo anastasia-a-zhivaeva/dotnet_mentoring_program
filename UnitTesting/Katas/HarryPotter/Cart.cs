50bb0b3e0c1307f75eebd60b7f957a657168db26
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-06-21 13:59:22 -0400

    Implement calculating cart total for 2 diff books

--- UnitTesting/Katas/HarryPotter/Cart.cs
+++ UnitTesting/Katas/HarryPotter/Cart.cs
@@ -2,6 +2,16 @@
 {
     internal class Cart
     {
+        private static readonly Dictionary<int, double> _discounts = new Dictionary<int, double>()
+        {
+            { 0, 0 },
+            { 1, 0 },
+            { 2, 0.05 },
+            { 3, 0.01 },
+            { 4, 0.2 },
+            { 5, 0.25 },
+        };
+
         public Cart()
         {
         }
@@ -11,13 +21,45 @@
         internal static double CalculateTotal(Cart cart)
         {
             double total = 0;
+            Dictionary<int, (Book book, int quantity)> books = new Dictionary<int, (Book book, int quantity)>();
 
-            foreach (Book book in cart.Books)
+            foreach (Book cartBook in cart.Books)
             {
-                total += book.Price;
+                (Book book, int quantity) value;
+                if (books.TryGetValue(cartBook.Order, out value))
+                {
+                    value.quantity += 1;
+                }
+                else
+                {
+                    books.Add(cartBook.Order, (cartBook, 1));
+                }
+            }
+
+            double discount = CalculateDiscount(books.Count);
+
+            foreach (KeyValuePair<int, (Book book, int quantity)> pair in books)
+            {
+                var book = pair.Value.book;
+                var quantity = pair.Value.quantity;
+
+                total += discount  >  0 ? book.Price * (1 - discount) : book.Price;
+
+                var quantityLeft = quantity - 1;
+                if (quantityLeft > 0)
+                {
+                    total += book.Price * quantityLeft;
+                }
             }
 
             return total;
+        }
+
+        private static double CalculateDiscount(int count)
+        {
+            double discount;
+            _discounts.TryGetValue(count, out discount);
+            return discount;
         }
     }
 }
