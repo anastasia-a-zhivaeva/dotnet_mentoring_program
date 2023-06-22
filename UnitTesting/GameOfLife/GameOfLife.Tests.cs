857dcef58cc2571bcb7c11541784b3d12d931870
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-06-22 14:59:39 -0400

    Create test for dead cell to become alive

--- UnitTesting/GameOfLife/GameOfLife.Tests.cs
+++ UnitTesting/GameOfLife/GameOfLife.Tests.cs
@@ -73,5 +73,37 @@ namespace GameOfLife
                 }
             }
         }
+
+        [Fact]
+        public void CalculateNextGeneration_DeadCellWith3AliveNeighboursBecomeAlive()
+        {
+            var dc = GameOfLifeProcessor.DeadCell;
+            var ac = GameOfLifeProcessor.AliveCell;
+            var cellsGrid = new string[4, 8]
+            {
+                { dc, dc, dc, dc, dc, dc, dc, dc },
+                { dc, dc, dc, dc, ac, dc, dc, dc },
+                { dc, dc, dc, ac, ac, dc, dc, dc },
+                { dc, dc, dc, dc, dc, dc, dc, dc },
+            };
+
+            string[,] result = GameOfLifeProcessor.CalculateNextGeneration(cellsGrid);
+
+
+            for (int x = 0; x < cellsGrid.GetLength(0); x++)
+            {
+                for (int y = 0; y < cellsGrid.GetLength(1); y++)
+                {
+                    if ((x == 1 && y == 3) || (x == 1 && y == 4) || (x == 2 && y == 3) || (x == 2 && y == 4))
+                    {
+                        Assert.Equal(ac, result[x, y]);
+                    }
+                    else
+                    {
+                        Assert.Equal(dc, result[x, y]);
+                    }
+                }
+            }
+        }
     }
 }
