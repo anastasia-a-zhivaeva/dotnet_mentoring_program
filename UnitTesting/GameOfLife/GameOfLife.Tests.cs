ba8b69e3347744ff6c319d88dcda1198e8a2b1ab
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-06-22 14:26:18 -0400

    Create test for alive cell to become dead
    Should become dead if more that 3 alive neighbours

--- UnitTesting/GameOfLife/GameOfLife.Tests.cs
+++ UnitTesting/GameOfLife/GameOfLife.Tests.cs
@@ -6,19 +6,19 @@ namespace GameOfLife
         public void CalculateNextGeneration_AllDeadCellsRemainDead()
         {
             var dc = GameOfLifeProcessor.DeadCell;
-            var cellsGrid = new string[3, 3] 
-            { 
-                { dc, dc, dc }, 
-                { dc, dc, dc }, 
-                { dc, dc, dc } 
+            var cellsGrid = new string[3, 3]
+            {
+                { dc, dc, dc },
+                { dc, dc, dc },
+                { dc, dc, dc }
             };
 
             string[,] result = GameOfLifeProcessor.CalculateNextGeneration(cellsGrid);
 
 
-            foreach(var cell in result)
+            foreach (var cell in result)
             {
-                Assert.Equal(".", cell);
+                Assert.Equal(dc, cell);
             }
         }
 
@@ -39,7 +39,38 @@ namespace GameOfLife
 
             foreach (var cell in result)
             {
-                Assert.Equal(".", cell);
+                Assert.Equal(dc, cell);
+            }
+        }
+
+        [Fact]
+        public void CalculateNextGeneration_AliveCellsWithMoreThan3AliveNeighboursBecomeDead()
+        {
+            var dc = GameOfLifeProcessor.DeadCell;
+            var ac = GameOfLifeProcessor.AliveCell;
+            var cellsGrid = new string[3, 3]
+            {
+                { dc, dc, dc },
+                { ac, ac, ac },
+                { dc, ac, dc }
+            };
+
+            string[,] result = GameOfLifeProcessor.CalculateNextGeneration(cellsGrid);
+
+
+            for (int x = 0; x < cellsGrid.GetLength(0); x++)
+            {
+                for (int y = 0; y < cellsGrid.GetLength(1); y++)
+                {
+                    if ((x == 1 && y == 0) || (x == 1 && y == 2) || (x == 2 && y == 1))
+                    {
+                        Assert.Equal(ac, cellsGrid[x, y]);
+                    }
+                    else
+                    {
+                        Assert.Equal(dc, cellsGrid[x, y]);
+                    }
+                }
             }
         }
     }
