0dc9c64d8aa4a753f7f14c7af3dea9845d6580f3
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-06-22 14:11:15 -0400

    Create test for alive cell to become dead
    Should become dead if less than 2 alive heighbours

--- UnitTesting/GameOfLife/GameOfLife.Tests.cs
+++ UnitTesting/GameOfLife/GameOfLife.Tests.cs
@@ -21,5 +21,26 @@ namespace GameOfLife
                 Assert.Equal(".", cell);
             }
         }
+
+        [Fact]
+        public void CalculateNextGeneration_AliveCellsWithLessThan2AliveNeighboursBecomeDead()
+        {
+            var dc = GameOfLifeProcessor.DeadCell;
+            var ac = GameOfLifeProcessor.AliveCell;
+            var cellsGrid = new string[3, 3]
+            {
+                { dc, dc, dc },
+                { dc, ac, dc },
+                { dc, dc, dc }
+            };
+
+            string[,] result = GameOfLifeProcessor.CalculateNextGeneration(cellsGrid);
+
+
+            foreach (var cell in result)
+            {
+                Assert.Equal(".", cell);
+            }
+        }
     }
 }
