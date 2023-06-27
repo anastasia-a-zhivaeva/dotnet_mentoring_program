ea3cf9b6b27caf23479a64226588b35d5b8d2655
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-06-27 19:07:13 -0400

    feat (SQLFundamentals): create EmployeeInfo View

---
+++ SQLFundamentals/SQLDB/Views/EmployeeInfo.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+	AS SELECT 
+			Employee.Id AS EmployeeId, 
+			(CASE WHEN Employee.EmployeeName IS NOT NULL THEN Employee.EmployeeName ELSE CONCAT(Person.FirstName, ' ', Person.LastName) END) AS EmployeeFullName,
+			CONCAT(Address.ZipCode, '_', Address.State, ', ', Address.City, '-', Address.Street) AS EmployeeFullAddress,
+			CONCAT(Employee.CompanyName, '(', Employee.Position, ')') AS EmployeeCompanyInfo
+	   FROM [Employee]
+	   INNER JOIN [Address] ON Address.Id = Employee.AddressId
+	   INNER JOIN [Person] ON Person.Id = Employee.PersonId;
