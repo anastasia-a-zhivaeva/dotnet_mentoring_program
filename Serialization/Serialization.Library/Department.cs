41e2f67bba3bfe89bf3f4d891dc32c0eeff03ff5
Anastasiia Zhivaeva <anastasia.a.zhivaeva@gmail.com>
2023-07-07 17:11:34 -0400

    feat (Serialization): use Binary, XML and JSON serializers

---
+++ Serialization/Serialization.Library/Department.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Serialization.Library
+{
+    [Serializable]
+    public class Department
+    {
+        [XmlElement("name")]
+        public string DepartmentName { get; set; }
+        [XmlArray("CurrentEmployees")]
+        [JsonPropertyName("CurrentEmployees")]
+        public List<Employee> Employees { get; set; }
+
+        public Department() 
+        {
+            DepartmentName = "Unknown department";
+            Employees = new List<Employee>();
+        }
+
+        public Department(string departmentName,  List<Employee> employees)
+        {
+            DepartmentName = departmentName;
+            Employees = employees;
+        }
+
+        public override string ToString()
+        {
+            var department = new StringBuilder(DepartmentName).AppendLine();
+            foreach (var employee in Employees)
+            {
+                department.AppendLine(employee.ToString());
+            }
+            return department.ToString();
+        }
+    }
+}
---
+++ Serialization/Serialization.Library/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Serialization.Library
+{
+    [Serializable]
+    public class Employee
+    {
+        [XmlAttribute("name")]
+        [JsonPropertyName("name")]  
+        public string EmployeeName { get; set; }
+
+        public Employee() 
+        {
+            EmployeeName = "Unknown employee";
+        }
+        public Employee(string employeeName)
+        {
+            EmployeeName = employeeName;
+        }
+
+        public override string ToString() => EmployeeName;
+    }
+}
